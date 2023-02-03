6b3f7bdfada5b7f7c011a086428f3a86209a8ce9
Rohan Godha <rgodha24@brophybroncos.org>
2023-02-03 12:51:27 -0700

    unocss + pink
    jng is artistically gifted (gay)
    
    Co-Authored-By: Jacob <110934976+jacobng24@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 import solidJs from "@astrojs/solid-js";
+import unocss from "unocss/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rohangodha.me/',
-  integrations: [mdx(), sitemap(), tailwind(), solidJs()]
+  site: "https://rohangodha.me/",
+  integrations: [mdx(), sitemap(), solidJs(), unocss({
+    
+  })],
 });

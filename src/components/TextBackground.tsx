0fea57dc8a411b2718415ca97962a18069e5db8a
Rohan Godha <rgodha24@brophybroncos.org>
2023-04-08 15:52:30 -0700

    changing letter background done

--- src/components/TextBackground.tsx
+++ src/components/TextBackground.tsx
@@ -1,3 +1,43 @@
+import { For, createSignal, onCleanup } from "solid-js";
+import TextLine from "./TextLine";
+
 export default function TextBackground() {
-  return <> </>
+   const [height, setHeight] = createSignal(window.innerHeight);
+   const [width, setWidth] = createSignal(window.innerWidth);
+
+   const letters = () => calculateLetterAmounts(height(), width()).letters;
+   const lines = () => calculateLetterAmounts(height(), width()).lines;
+
+   function handleResize() {
+      setHeight(window.innerHeight);
+      setWidth(window.innerWidth);
+   }
+
+   window.addEventListener("resize", handleResize);
+   onCleanup(() => window.removeEventListener("resize", handleResize));
+
+   return (
+     <div
+       class="font-mono max-w-full overflow-hidden text-slate-800"
+         style={{
+            "line-height": "20px",
+            "font-size": "16px",
+         }}
+      >
+         <For each={new Array(lines())}>
+            {() => (
+               <>
+                  <TextLine letters={letters} />
+                  <br />
+               </>
+            )}
+         </For>
+      </div>
+   );
+}
+
+function calculateLetterAmounts(height: number, width: number): { lines: number; letters: number } {
+   const lines = Math.floor(height / 20);
+   const letters = Math.floor(width / 9.5);
+   return { lines, letters };
 }
--- src/components/TextLine.tsx
+++ src/components/TextLine.tsx
@@ -1,3 +1,24 @@
-export default function TextLine() {
-   return <></>;
+import { Component, For, createSignal, from, onMount } from "solid-js";
+
+const TextLine: Component<{ letters: () => number }> = ({ letters }) => {
+   const line = from((set) => {
+      const t = setInterval(() => set(generateLine()), 200);
+      return () => clearInterval(t);
+   });
+
+   function generateLine() {
+      let line = "";
+      for (let i = 0; i < letters(); i++) {
+         line += randomLetter();
+      }
+      return line;
+   }
+
+   return <>{line()}</>;
+};
+
+export default TextLine;
+
+function randomLetter() {
+   return String.fromCharCode(97 + Math.floor(Math.random() * 26));
 }

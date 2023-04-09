acd0e5b59799a14c7e7436564b189e6726bb521d
Rohan Godha <rgodha24@brophybroncos.org>
2023-04-08 19:25:55 -0700

    waterfall text ignores stuff

--- src/components/TextBackground.tsx
+++ src/components/TextBackground.tsx
@@ -1,12 +1,19 @@
-import { For, createSignal, from, onCleanup } from "solid-js";
+import { For, createMemo, createSignal, from } from "solid-js";
 
 export default function TextBackground() {
    const [height, setHeight] = createSignal(window.innerHeight);
    const [width, setWidth] = createSignal(window.innerWidth);
+
    const letters = () => calculateLetterAmounts(height(), width()).letters;
    const lines = () => calculateLetterAmounts(height(), width()).lines;
 
-   const text = from<string[]>((set) => {
+   const hidden = createMemo(() => {
+      const ignored = Array.from(document.querySelectorAll("#ignored"));
+
+      return getHiddenFromIgnored(ignored, lines, letters);
+   });
+
+   const text = from<string[][]>((set) => {
       const interval = setInterval(() => {
          set((prev) => {
             const newlines = lines() - (prev?.length || 0) + 1;
@@ -21,13 +28,14 @@ export default function TextBackground() {
                prev?.push(
                   Array.from({ length: letters() }, () =>
                      String.fromCharCode(Math.floor(Math.random() * 26) + 97)
-                  ).join("")
+                  )
                );
             }
 
             return prev;
          });
       }, 100);
+
       function handleResize() {
          set(undefined);
          setHeight(window.innerHeight);
@@ -44,7 +52,7 @@ export default function TextBackground() {
 
    return (
       <div
-         class="font-mono max-w-full overflow-hidden gradient-text bg-gradient-to-t bg-gradient-from-coolgray-900 bg-gradient-to-slate-700"
+         class=" max-w-full overflow-hidden gradient-text bg-gradient-to-t bg-gradient-from-coolgray-900 bg-gradient-to-slate-700 font-mono whitespace-pre"
          style={{
             "line-height": "20px",
             "font-size": "16px",
@@ -52,19 +60,81 @@ export default function TextBackground() {
          aria-hidden={true}
       >
          <For each={text()}>
-            {(line) => (
-               <>
-                  {line}
-                  <br />
-               </>
-            )}
+            {(line, y) => {
+               return (
+                  <>
+                     {line
+                        .map((letter, x) => {
+                           const isHidden = (hidden()[y()] || [])[x];
+                           return isHidden ? " " : letter;
+                        })
+                        .join("")}
+                     <br />
+                  </>
+               );
+            }}
          </For>
       </div>
    );
 }
 
+const LINE_HEIGHT = 20;
+const LETTER_WIDTH = 9.7;
+
 function calculateLetterAmounts(height: number, width: number): { lines: number; letters: number } {
-   const lines = Math.floor(height / 20);
-   const letters = Math.floor(width / 9.5);
+   const lines = Math.floor(height / LINE_HEIGHT);
+   const letters = Math.floor(width / LETTER_WIDTH) + 1;
    return { lines, letters };
+}
+
+type Point = [number, number];
+
+function getHiddenFromIgnored(ignored: Element[], lines: () => number, letters: () => number) {
+   const hidden: boolean[][] = [];
+
+   for (let i = 0; i < lines(); i++) {
+      hidden[i] = [];
+      for (let j = 0; j < letters(); j++) {
+         hidden[i][j] = false;
+      }
+   }
+
+   const elipses: ((point: Point) => void)[] = [];
+
+   ignored.forEach((el) => {
+      const rect = el.getBoundingClientRect();
+      const top = Math.ceil(rect.top / LINE_HEIGHT) - 1;
+      const left = Math.ceil(rect.left / LETTER_WIDTH);
+      const bottom = Math.floor(rect.bottom / LINE_HEIGHT);
+      const right = Math.floor(rect.right / LETTER_WIDTH);
+
+      const dx = right - left;
+      const dy = bottom - top;
+
+      const centery = top + dy / 2;
+      const centerx = left + dx / 2;
+
+      // TODO: Fix this
+      elipses.push((point) => {
+         const [x, y] = point;
+
+         const dx2 = x - centerx;
+         const dy2 = y - centery;
+
+         const a = dx / 2 + 5;
+         const b = dy / 2 + 2;
+
+         const inside = (dx2 * dx2) / (a * a) + (dy2 * dy2) / (b * b) <= 1;
+
+         return inside;
+      });
+   });
+
+   for (let i = 0; i < lines(); i++) {
+      for (let j = 0; j < letters(); j++) {
+         hidden[i][j] = elipses.some((elipse) => elipse([j, i]));
+      }
+   }
+
+   return hidden;
 }

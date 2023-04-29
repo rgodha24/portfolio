ccf215067e0d8da65e93311a12390e2b49d18ac4
Rohan Godha <rgodha24@brophybroncos.org>
2023-04-29 16:55:01 -0700

    better ignored state for landing page

--- src/components/TextBackground.tsx
+++ src/components/TextBackground.tsx
@@ -1,21 +1,15 @@
-import { For, createEffect, createMemo, createSignal, from, onMount } from "solid-js";
+import { For, createSignal, from } from "solid-js";
+import "../styles/ignored.css";
 
 const LINE_HEIGHT = 20;
 let LETTER_WIDTH = 8.898;
 
 export default function TextBackground() {
-   const body = document.getElementsByTagName("body")[0];
    const [height, setHeight] = createSignal(window.outerHeight);
    const [width, setWidth] = createSignal(window.outerWidth);
 
    const letters = () => calculateLetterAmounts(height(), width()).letters;
    const lines = () => calculateLetterAmounts(height(), width()).lines;
-
-   const hidden = createMemo(() => {
-      const ignored = Array.from(document.querySelectorAll("#ignored"));
-
-      return getHiddenFromIgnored(ignored, lines, letters);
-   });
 
    const text = from<string[][]>((set) => {
       const interval = setInterval(() => {
@@ -63,15 +57,10 @@ export default function TextBackground() {
          aria-hidden={true}
       >
          <For each={text()}>
-            {(line, y) => {
+            {(line) => {
                return (
                   <>
-                     {line
-                        .map((letter, x) => {
-                           const isHidden = (hidden()[y()] || [])[x];
-                           return isHidden ? " " : letter;
-                        })
-                        .join("")}
+                     {line.join("")}
                      <br />
                   </>
                );
@@ -86,56 +75,4 @@ function calculateLetterAmounts(height: number, width: number): { lines: number;
    const letters = Math.floor(width / LETTER_WIDTH) + 1 - (14 / 1440) * width;
 
    return { lines, letters };
-}
-
-type Point = [number, number];
-
-function getHiddenFromIgnored(ignored: Element[], lines: () => number, letters: () => number) {
-   const hidden: boolean[][] = [];
-
-   for (let i = 0; i < lines(); i++) {
-      hidden[i] = [];
-      for (let j = 0; j < letters(); j++) {
-         hidden[i][j] = false;
-      }
-   }
-
-   const elipses: ((point: Point) => void)[] = [];
-
-   ignored.forEach((el) => {
-      const rect = el.getBoundingClientRect();
-      const top = Math.ceil(rect.top / LINE_HEIGHT);
-      const left = Math.ceil(rect.left / LETTER_WIDTH);
-      const bottom = Math.floor(rect.bottom / LINE_HEIGHT);
-      const right = Math.floor(rect.right / LETTER_WIDTH);
-
-      const dx = right - left;
-      const dy = bottom - top;
-
-      const centery = top + dy / 2;
-      const centerx = left + dx / 2.8;
-
-      // TODO: Fix this
-      elipses.push((point) => {
-         const [x, y] = point;
-
-         const dx2 = x - centerx;
-         const dy2 = y - centery;
-
-         const a = dx / 2 + dx / 4;
-         const b = dy / 2 + dy / 2.8;
-
-         const inside = (dx2 * dx2) / (a * a) + (dy2 * dy2) / (b * b) <= 1;
-
-         return inside;
-      });
-   });
-
-   for (let i = 0; i < lines(); i++) {
-      for (let j = 0; j < letters(); j++) {
-         hidden[i][j] = elipses.some((elipse) => elipse([j, i]));
-      }
-   }
-
-   return hidden;
 }

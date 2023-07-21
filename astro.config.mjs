ba59d3934cf0c1b74d3d8cd1f1087fcd8af797d7
Rohan Godha <rgodha24@brophybroncos.org>
2023-07-21 10:26:37 -0700

    astro 2.9 + view transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,16 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 import solidJs from "@astrojs/solid-js";
 import unocss from "unocss/astro";
-
-// https://astro.build/config
 import vercel from "@astrojs/vercel/static";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://rohan.zip",
   integrations: [mdx(), sitemap(), solidJs(), unocss()],
   output: "static",
   adapter: vercel({}),
+  experimental: {
+    viewTransitions: true,
+  },
 });

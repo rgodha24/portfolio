cb7fc613ccb78d0d878e344b52362356dff90790
Rohan Godha <rgodha24@brophybroncos.org>
2024-05-15 10:53:15 -0700

    formatting + update lockfile

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,22 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import solidJs from "@astrojs/solid-js";
 import unocss from "unocss/astro";
-import cloudflare from "@astrojs/cloudflare"
+import cloudflare from "@astrojs/cloudflare";
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.rohangodha.com",
-  integrations: [mdx(), sitemap(), solidJs(), unocss({
-    mode: "global",
-    injectReset: true,
-  }), prefetch()],
-  output: "hybrid",
-  adapter: cloudflare({imageService: "compile"})
+   site: "https://www.rohangodha.com",
+   integrations: [
+      mdx(),
+      sitemap(),
+      solidJs(),
+      unocss({
+         mode: "global",
+         injectReset: true,
+      }),
+      prefetch(),
+   ],
+   output: "hybrid",
+   adapter: cloudflare({ imageService: "compile" }),
 });

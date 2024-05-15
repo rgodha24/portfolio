79890813ebfbb0837fe61c16e1ede1042a83b037
Rohan Godha <rgodha24@brophybroncos.org>
2024-05-15 08:59:31 -0700

    move to CF

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,15 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import solidJs from "@astrojs/solid-js";
 import unocss from "unocss/astro";
-import vercel from "@astrojs/vercel/static";
+import cloudflare from "@astrojs/cloudflare"
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.rgodha.com",
+  site: "https://www.rohangodha.com",
   integrations: [mdx(), sitemap(), solidJs(), unocss({
     mode: "global"
   }), prefetch()],
-  output: "static",
-  adapter: vercel({})
+  output: "hybrid",
+  adapter: cloudflare({imageService: "compile"})
 });
